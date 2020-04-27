907b57cd14f0e1c92439b79278dfd9a6af7f026d
cocokaribou <younginlee2017@gmail.com>
2020-04-27 15:04:38 +0900

    0427 java quiz

---
+++ MethodEx1.java
@@ -0,0 +1,13 @@
+class MethodEx1 
+{
+	public static int tryout = 100;
+
+
+	public static void main(String[] args) 
+	{
+
+		Calc_method c = new Calc_method();
+		int num = c.calcSum(1,2);
+		System.out.println(num);
+	}
+}
