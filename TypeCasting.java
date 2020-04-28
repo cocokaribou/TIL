90b2263b3f777f4e92d714a7670dade2ce2ac69b
cocokaribou <younginlee2017@gmail.com>
2020-04-28 15:20:28 +0900

    Character variables editted

--- TypeCasting.java
+++ TypeCasting.java
@@ -4,49 +4,58 @@ class  TypeCasting
 	{		
 		float flt = 999999999999999L;
 		System.out.println(flt); 
-			//출력결과 9.9999999E14
-			//long → float 자동형변환
+			//9.9999999E14
+			//long → float auto
 
 
 		//long lng = 3.14f;
 		//System.out.println(lng);
-			//에러
-			//float → long 자동형변환 불가!
+			//error!
 
 		//int nummm = 3.14f; System.out.println(nummm);
 
 		long lng = (long)3.14f;			System.out.println(lng);
-			//출력결과 3(L)
-			//(long)float 강제형변환
+			//3(L)
+			//(long)float cast
 
 		
 		int chr_to_int = 'A';			System.out.println(chr_to_int);
-					//출력결과 65
+					//65
 		
 		short chr_to_shrt = 'A';		System.out.println(chr_to_shrt);
-					//출력결과 65
+					//65
 			
 		char int_to_chr = 65;			System.out.println(int_to_chr);
-					//출력결과 'A'
-					//char 범위 안.
+					//'A'
 
-		char int_to_chr2 = 65536;		System.out.println(int_to_chr2);
-					//에러!
-					//char 범위 밖.
+		char ansi_1 = 125;			System.out.println(ansi_1);
+		char ansi_2 = 126;			System.out.println(ansi_2);
+		char ansi_3 = 127;			System.out.println(ansi_3);
+		char ansi_4 = 128;			System.out.println(ansi_4);
 
-		char int_to_char3 = -10;	
-					//에러!
-					//char 범위 밖.
+		char ansi_5 = 255;			System.out.println(ansi_5);
+		char ansi_6 = 256;			System.out.println(ansi_6);
+		char ansi_7 = 257;			System.out.println(ansi_7);
+
+//		char int_to_chr2 = 65536;		System.out.println(int_to_chr2);
+//					//error!
+//					//beyond char range
+
+//		char int_to_char3 = -11;	
+//					//error!
+//					//below char range
 					
 
 		int lng_to_int = (int)400L;		System.out.println(lng_to_int);
-					//출력결과 400
+					//400
 
 		int lng_to_int2 = (int)2147483648L; System.out.println(lng_to_int2);
-					//오버플로우!
+					//overflow!
+					//-2147483648
 
 		byte int_to_byte = (byte)128;		System.out.println(int_to_byte);
-					//오버플로우!
+					//overflow!
+					//-128
 
 	}
 }
