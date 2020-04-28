0046a043c8b6a18ebd9b2d0ef050892a2769ec54
cocokaribou <younginlee2017@gmail.com>
2020-04-28 15:38:40 +0900

    editted comments about Boolean(String s) constructor

--- testing_boolean.java
+++ testing_boolean.java
@@ -11,12 +11,12 @@ class testing_boolean
 
 		System.out.println("\nString: "+s1+"\nBoolean: "+bl);
 		
-		//Boolean 클래스의 생성자 Boolean(String s);
+		//Boolean class constructor: Boolean(String s);
 		//String converted to the Boolean.
 
 		//new Boolean("True") produces a Boolean object that represents true.
 		//new Boolean("False") produces a Boolean object that represents false.
-		//"true"나 "false" 이외의 string은 전부 false가 뜬다.
+		//Every String object except "true" & "false" == false
 
 
 	}
