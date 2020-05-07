74fc1827bc983eccd246d35a65410c5b62b296dc
cocokaribou <younginlee2017@gmail.com>
2020-05-07 15:02:19 +0900

    new project file

---
+++ 2020.05/SCIT_39TH/src/com.sesoc.www.day0507/Car.java
@@ -0,0 +1,10 @@
+package com.sesoc.www.day0507;
+
+public class Car {
+    String name;
+
+    public Car (String name) {
+        this.name = name;
+    }
+    
+}
---
+++ 2020.05/SCIT_39TH/src/com.sesoc.www.day0507/CarFactory.java
@@ -0,0 +1,8 @@
+package com.sesoc.www.day0507;
+
+public class CarFactory {
+    public static void main(String[] args)) {
+        Car car1 = new Car("BMW");
+        Car car2 = new Car("Sonata");
+    }
+}
