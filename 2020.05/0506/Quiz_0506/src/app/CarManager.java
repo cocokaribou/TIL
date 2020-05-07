5b66e2d77dcb3fa145caa5738ac8969e9c1fd708
cocokaribou <younginlee2017@gmail.com>
2020-05-08 08:19:25 +0900

    editted query logic

--- 2020.05/0506/Quiz_0506/src/app/CarManager.java
+++ 2020.05/0506/Quiz_0506/src/app/CarManager.java
@@ -11,29 +11,33 @@ public class CarManager {
     }
 
     boolean tellIfNull(){
-        if (counter==0) {
+        if (counter==0 && array[0]==null) {
             return false;}
         else
             return true;
     }
 
-    void infoQuery(String number){      
+    void infoQuery(String number){
+        boolean flag = false;
         for (int i=0; i<counter; i++){
             if (array[i].contact_num.equals(number)){
+                flag = true;
                 System.out.println();
                 System.out.println("[no. "+(i+1)+"]");
                 System.out.println("NAME : "+array[i].car_owner);
                 System.out.println("CAR NUMBER : "+array[i].car_num);
                 System.out.println("CONTACT NUMBER : "+array[i].contact_num);
                 System.out.println("CAR LOCATION : "+array[i].car_location);
+                break;
             }
-            else {
+        }
+        if (flag == false){
                 System.out.println();
                 System.out.println("no matching car info!");
                 System.out.println("please try again!");
-            }
-                
         }
+              
+        
     }
 
     void printInfo(){
