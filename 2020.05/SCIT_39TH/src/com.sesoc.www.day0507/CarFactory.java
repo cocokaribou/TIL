a335154b1643827f19a753114a4748831c8747b1
cocokaribou <younginlee2017@gmail.com>
2020-05-08 11:33:27 +0900

    deleted file

--- 2020.05/SCIT_39TH/src/com.sesoc.www.day0507/CarFactory.java
+++
@@ -1,8 +0,0 @@
-package com.sesoc.www.day0507;
-
-public class CarFactory {
-    public static void main(String[] args)) {
-        Car car1 = new Car("BMW");
-        Car car2 = new Car("Sonata");
-    }
-}
