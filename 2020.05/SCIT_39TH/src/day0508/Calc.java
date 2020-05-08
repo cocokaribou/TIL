52347e153e93556887a68daa9373fec70b5456eb
cocokaribou <younginlee2017@gmail.com>
2020-05-08 11:34:15 +0900

    added files

---
+++ 2020.05/SCIT_39TH/src/day0508/Calc.java
@@ -0,0 +1,36 @@
+package day0508;
+
+public class Calc {
+
+    public int sum (int num1, int num2){
+        int i = num1 + num2;
+        return i;
+    }
+
+    public int minus (int num1, int num2){
+        int i = num1 - num2;
+        return i;
+    }
+
+    public int multiply (int num1, int num2){
+        int i = num1 * num2;
+        return i;
+    }
+
+    public int dividing (int num1, int num2){
+        double i = num1 / num2;
+        return (int)i;
+    }
+
+    public void printMenu(){
+        System.out.println("==================");
+        System.out.println("1. addition");
+        System.out.println("2. substraction");
+        System.out.println("3. multiplication");
+        System.out.println("4. division");
+        System.out.println("0. Exit");
+        System.out.println("==================");
+    }
+
+
+}
