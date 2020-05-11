41e0ca33d1451a6c58e9068a8944d8134ce69d49
cocokaribou <younginlee2017@gmail.com>
2020-05-11 11:44:49 +0900

    added package directory

---
+++ 2020.05/SCIT_39TH/src/com/sesoc/www/hw/packagetest.java
@@ -0,0 +1,9 @@
+package com.sesoc.www.hw;
+
+public class packagetest {
+   
+    public static void main (String[] args) {
+        System.out.println("declaring package in vscode: you have to follow the acutal local directory");
+    }
+    
+}
---
+++ 2020.05/SCIT_39TH/src/com/sesoc/www/model/pacakagetest.java
@@ -0,0 +1,7 @@
+package com.sesoc.www.model;
+
+public class pacakagetest {
+    public static void main(String[] args){
+        System.out.println("now it works!");
+    }
+}
