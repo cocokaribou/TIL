fb7d3800ae2f1b5e04f117868a4ab6248c5a4ede
cocokaribou <younginlee2017@gmail.com>
2020-05-12 09:04:57 +0900

    day0512 editted comment on getter & setter method

--- 2020.05/SCIT_39TH/src/day0511_main/TimeMain.java
+++ 2020.05/SCIT_39TH/src/day0511_main/TimeMain.java
@@ -8,9 +8,9 @@ public class TimeMain {
 
 
 //if instance variables were declared public: 
-//first you import the package(if they are located in a different pacakge),
-//then you can directly access them via class instance.
-//...which is not recommended for the sake of encapsulation.
+//first you import the package (if the class is located in a different pacakge),
+//then you can directly access to them via class instance.
+//...which is not recommended.
 
         // Time t = new Time();
         // t.hour = 1;
@@ -21,15 +21,14 @@ public class TimeMain {
 
 
 //if instance variables were declared private:
-//first you import the package,
-//then you call constructors / methods to access them
+//call constructors / methods to access them
 
 //constructor
 
         Time t = new Time(1, 20, 59);
         Time t2 = new Time(2, 20, 59);
 
-//setter method to change the value of the instance variable after when objects were made        
+//setter methods to change the value of the instance variables after when objects were made        
         
         t.setHour(2);
         t2.setMinute(15);
@@ -38,7 +37,7 @@ public class TimeMain {
         System.out.printf("%d h %d m %d s%n", t2.getHour(), t2.getMinute(), t2.getSecond()); //2 h 15 m 59 s
         
 
-//if you set the instance value of the Time object out of range (ex t.setSecond(60));
+//if you set the value of the instance variable out of range (ex t.setSecond(60));
 //it will print out error message you've written in the setter method
 //and then get 0 (default value of the instance variable).
 
@@ -49,9 +48,6 @@ public class TimeMain {
         t3.setSecond(60);
         System.out.printf("%d h %d m %d s%n", t3.getHour(), t3.getMinute(), t3.getSecond()); //wrong input! 0 h 0 m 0 s
         
-
-
-
     }
     
 }
