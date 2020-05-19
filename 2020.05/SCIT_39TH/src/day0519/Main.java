325d8a2df6b6f42737cc4b9126205a9215fe0f64
cocokaribou <younginlee2017@gmail.com>
2020-05-19 16:57:15 +0900

    day0519

--- 2020.05/SCIT_39TH/src/day0519/Main.java
+++ 2020.05/SCIT_39TH/src/day0519/Main.java
@@ -2,27 +2,60 @@ package day0519;
 
 public class Main {
     public static void main(String[] args) {
-
-//subclas
-        Human h1 = new Human(24, "female", "lee");
-        h1.eat();
-        h1.move();
-        h1.speak();
-
+        
 //superclass
-        Animal a1 = new Animal(5, "female");
+        Animal a = new Animal(5, "female");
+        a.eat();
+        a.move();
+
+//subclass
+        Human h = new Human(24, "female", "lee");
         //overridden methods
-        a1.eat();
-        a1.move();
+        h.eat();
+        h.move();
+        h.speak();
+
 
 //up-casting
-        //Animal a2 = (Animal)h1; //explicit class cast
-        Animal a2 = h1; //implicit class cast
+        
+        Animal a1 = (Animal)h; //explicit class cast
+        //Animal a1 = h1; //implicit class cast
 
-        //a2.speak();
+        //a1.getName();
+        //a1.speak();
         //the subclass method is underdefined for the superclass type
 
-        a2.eat();
+        a1.eat(); //calling overridden method from subclass
+
+//down-casting an up-casted object
+        //only allows explicit class cast
+        Human h1 = (Human)a1;
+
+        h1.speak();
+
+//another subclass
+        Dog d = new Dog(5, "male");
+
+
+//superclass type array
+        Animal[] animal_Arr = new Animal[100];
+        animal_Arr[0] = h1; //Human object upcasted to Animal object
+        animal_Arr[1] = d;  //Dog object upcasted to Animal object
+
+        //Dog dd = (Dog)animal_Arr[0];
+        //runtime exception
+        //ClassCastException
+        
+//instanceof operator, return boolean value.
+        if(animal_Arr[0] instanceof Human){
+            System.out.println("Human was upcasted to Animal");
+            Human h2 = (Human)animal_Arr[0];
+            h2.speak();
+        }else if(animal_Arr[0] instanceof Dog){
+            System.out.println("Dog was upcasted to Animal");
+            Dog dd = (Dog)animal_Arr[0];
+            dd.bark();
+        }
 
 
     }
