76037211da5324ecefc27a3847da28676b878227
cocokaribou <younginlee2017@gmail.com>
2020-06-10 09:53:10 +0900

    day0607 SQL quiz

--- 2020.06/day0607/hr_quiz2.sql
+++ 2020.06/day0607/hr_quiz2.sql
@@ -9,13 +9,14 @@ SELECT department_id AS 부서번호,
 FROM EMPLOYEES GROUP BY department_id
 ORDER BY department_id DESC;
 
+select * from user_constraints;
 
 SELECT department_id AS 부서번호, 
     COUNT(*) AS 사원수, 
     SUM(salary) AS 급여합계
 FROM EMPLOYEES
 GROUP BY department_id
-HAVING count(department_id) > 4;
+HAVING count(employee_id) > 4;
 
 SELECT job_id AS 업무ID,
     SUM(salary) AS 급여합계
@@ -24,13 +25,35 @@ WHERE job_id NOT LIKE 'SA%'
 GROUP BY job_id
 HAVING SUM(salary) > 10000;
 
+--using subquery
 SELECT e1.employee_id AS 사원번호,
     e1.first_name AS 이름,
     e1.manager_id AS "상사의 사원번호",
     e2.first_name AS "상사의 이름"
 FROM employees e1, employees e2
-WHERE e2.first_name IN (
+WHERE e2.first_name = (
     SELECT first_name
     FROM employees
     WHERE employee_id = 108
 )AND e1.manager_id = 108;
+
+--using table join (self join)
+SELECT e1.employee_id AS 사원번호,
+    e1.first_name AS 이름,
+    e1.manager_id AS "상사의 사원번호",
+    e2.first_name AS "상사의 이름"
+FROM employees e1, employees e2
+WHERE e1.manager_id = 108
+	AND e1.manager_id = e2.employee_id;
+    
+    
+--using table join (right outer join)
+SELECT e1.employee_id AS 사원번호,
+    e1.first_name AS 이름,
+    e1.manager_id AS "상사의 사원번호",
+    e2.first_name AS "상사의 이름"
+FROM employees e1, employees e2
+WHERE e1.manager_id = 108
+	AND e1.manager_id(+) = e2.employee_id;
+    
+--just in case theres any null value
